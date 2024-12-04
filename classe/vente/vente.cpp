9ce189311ebabdb2a4f1bb33e51dcb713af5806e
Gookd <127061216+Gookd@users.noreply.github.com>
2024-12-04 15:57:21 +0000

    Update : Constructeurs et getter/setters vente.cpp

--- classe/vente/vente.cpp
+++ classe/vente/vente.cpp
@@ -3,3 +3,41 @@ Enregistre les ventes du magasin avant de les écrire dans un autre fichier :
 - Ecrit sous forme de tableau avec le nom du produit, le prix à l'unité, la quantité et la date
 */
 
+#include "vente.hpp"
+#include <regex>
+
+// Constructeur
+Vente::Vente(const Produit& produit, int prix, int quantite, const std::string& dateVente)
+    : m_produit(produit), m_quantite(quantite), m_dateVente(dateVente) {
+    // Vérification des assertions pour valider les entrées
+
+    assert(m_quantite>0);
+        std::cout<<"La quantité du produit doit être supérieure à 0"<<std::endl;
+    
+    std::regex dateRegex(R"(^\d{2}/\d{2}/\d{4}$)");
+    assert(std::regex_match(m_dateVente,dateRegex));
+        std::cout<<"la date doit être au format jj/mm/aaaa"<<std::endl;
+}
+
+// Setters et getters
+void Vente::setPrixTTC(int prixTTC) {
+    m_prixTTC = prixTTC;
+}
+int Vente::getPrixTTC() const {
+    return m_prixTTC;
+}
+
+void Vente::setQuantite(int quantite) {
+    m_quantite = quantite;
+}
+int Vente::getQuantite() const {
+    return m_quantite;
+}
+
+
+void Vente::setDateVente(const std::string& dateVente) {
+    m_dateVente = dateVente;
+}
+const std::string& Vente::getDateVente() const {
+    return m_dateVente;
+}
--- classe/vente/vente.hpp
+++ classe/vente/vente.hpp
@@ -11,15 +11,16 @@ private :
     Produit m_produit;
     int m_quantite;
     std::string m_dateVente;
+    int m_prixTTC;
 
 public : 
 // Constructeur
-    Vente(const Produit& produit, int prix, int m_quantite, const std::string& m_dateVente);
+    Vente(const Produit& produit, int prixTTC, int m_quantite, const std::string& m_dateVente);
 
 // Getters et Setters
 
-    void setPrix(int prix);
-    int getPrix() const;
+    void setPrixTTC(int prixTTC);
+    int getPrixTTC() const;
 
     void setQuantite(int quantite);
     int getQuantite() const;
